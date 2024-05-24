6d31fbebe9ea44b282ae153e4a0ec90d6ebbce75
Liliane Lukong <liliane.haniel@gmail.com>
2024-05-24 21:18:25 +0100

    Header border for smaller screens fixed position

--- script.js
+++ script.js
@@ -4,5 +4,25 @@ document.addEventListener('DOMContentLoaded', function() {
 
     hamburgerMenu.addEventListener('click', () => {
         navLinks.classList.toggle('nav-active');
+        const expanded = hamburgerMenu.getAttribute('aria-expanded') === 'true' || false;
+        hamburgerMenu.setAttribute('aria-expanded', String(!expanded));
     });
 });
+
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('nav-active');
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const expanded = hamburgerMenu.getAttribute('aria-expanded') === 'true' || false;
+    hamburgerMenu.setAttribute('aria-expanded', String(!expanded));
+}
+
+// Function to adjust carousel container size based on header height
+function adjustCarouselContainer() {
+    const headerHeight = document.querySelector('header').offsetHeight;
+    const carouselContainer = document.querySelector('.carousel-container');
+    carouselContainer.style.marginTop = headerHeight + 'px';
+}
+
+// Event listener for changes in header size
+window.addEventListener('resize', adjustCarouselContainer);
